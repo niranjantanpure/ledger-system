7f9c5dd19c8d91e8eddd57f2a5c2099892137957
Niranjan Tanpure <niranjan8265@gmail.com>
2026-03-25 12:41:40 +0000

    Remove transaction module and externalize DB config

--- src/main/java/com/niranjan/ledger/transaction/LedgerTransactionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.niranjan.ledger.transaction;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LedgerTransactionRepository extends JpaRepository<LedgerTransaction, Long> {
-}
--- src/main/java/com/niranjan/ledger/transaction/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.niranjan.ledger.transaction;
-
-public enum TransactionType {
-    INCOME,
-    EXPENSE
-}

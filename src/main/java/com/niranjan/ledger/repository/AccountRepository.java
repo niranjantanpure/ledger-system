9b781b156016dd12aa2d003a62cd7940a573933f
Niranjan Tanpure <niranjan8265@gmail.com>
2026-03-27 15:21:53 +0000

    Implement AccountRepository and TransactionRepository using Spring Data JPA

--- src/main/java/com/niranjan/ledger/repository/AccountRepository.java
+++ src/main/java/com/niranjan/ledger/repository/AccountRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long>{
-    Optional<Account> findByAcccountNumber(String accountNumber);
+    Optional<Account> findByAccountNumber(String accountNumber);
 }

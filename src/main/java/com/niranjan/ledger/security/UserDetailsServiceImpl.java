3316d7193839fbd0f138cef93b32fbaf230a9f80
Niranjan Tanpure <niranjan8265@gmail.com>
2026-04-18 22:25:20 +0100

    feat: implement JWT-based authentication and user management system with global exception handling

---
+++ src/main/java/com/niranjan/ledger/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.niranjan.ledger.security;
+
+import com.niranjan.ledger.entity.User;
+import com.niranjan.ledger.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User appUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(appUser.getUsername())
+                .password(appUser.getPassword())
+                .authorities(appUser.getRole())
+                .build();
+    }
+}
